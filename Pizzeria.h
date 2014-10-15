f2a06259bfce2ef6618bd0644bc28bd8dc568ee2
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-10-15 16:10:33 -0500

    Finished plist and set up Pizzeria Class

---
+++ Pizzeria.h
@@ -0,0 +1,13 @@
+//
+//  Pizzeria.h
+//  ZaHunter
+//
+//  Created by Christopher on 10/15/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pizzeria : NSObject
+
+@end
---
+++ Pizzeria.m
@@ -0,0 +1,13 @@
+//
+//  Pizzeria.m
+//  ZaHunter
+//
+//  Created by Christopher on 10/15/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "Pizzeria.h"
+
+@implementation Pizzeria
+
+@end
