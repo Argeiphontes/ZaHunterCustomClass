c90cda00b302823cd6413525589b2957581c5098
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-10-15 14:31:42 -0500

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Christopher on 10/15/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by Christopher on 10/15/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
