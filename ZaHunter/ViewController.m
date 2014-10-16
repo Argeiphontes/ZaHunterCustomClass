51bcc2059477e49ab0ffb8971df038bb2a4b9b04
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-10-15 23:37:24 -0500

    Got a table :)

--- ZaHunter/ViewController.m
+++ ZaHunter/ViewController.m
@@ -9,10 +9,13 @@
 #import "ViewController.h"
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import "Pizzeria.h"
 
 @interface ViewController () <CLLocationManagerDelegate, UITableViewDataSource, UITableViewDelegate>
 
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property CLLocationManager *myLocationManager;
+@property NSMutableArray *pizzerias;
 
 @end
 
@@ -20,23 +23,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.pizzerias = [NSMutableArray array];
+    self.myLocationManager = [[CLLocationManager alloc]init];
+    [self.myLocationManager requestWhenInUseAuthorization];
+    self.myLocationManager.delegate = self;
 }
 
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return self.pizzerias.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [UITableViewCell new];
-    return cell;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    if (self.pizzerias.count) {
+
+        MKMapItem *pizzeria = [self.pizzerias objectAtIndex:indexPath.row];
+        // If pizzeria distance is within range show it. We want it closer than 6.21371 miles or 10 kilometers
+        cell.textLabel.text = pizzeria.name;
+//        cell.detailTextLabel.text = pizzeria.distanceFromCurrentLocationInMilesString;
+    }
+        return cell;
 }
 
 - (IBAction)huntZaButtonPressed:(id)sender
 {
-
+    [self.myLocationManager startUpdatingLocation];
+    NSLog(@"Locating you...");
 }
+
+// find user location
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    for (CLLocation *location in locations) {
+        if (location.verticalAccuracy < 1000 && location.horizontalAccuracy < 1000) {
+            [self findPizzaNearby: location];
+            NSLog(@"The Location is %@", location);
+            [self.myLocationManager stopUpdatingLocation];
+            break;
+
+        }
+    }
+}
+
+// find pizza locations
+-(void) findPizzaNearby: (CLLocation *) location
+{
+    MKLocalSearchRequest *request = [MKLocalSearchRequest new];
+    request.naturalLanguageQuery = @"pizza";
+    request.region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(5, 5));
+    MKLocalSearch *search = [[MKLocalSearch alloc]initWithRequest:request];
+    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+        NSArray *mapItems = response.mapItems;
+        [self.pizzerias addObjectsFromArray:mapItems];
+        NSLog(@"Map Items: %@ ", mapItems);
+        [self.tableView reloadData];
+
+    }];
+}
+
+
 
 @end
